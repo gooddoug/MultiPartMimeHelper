ca57eb9cd200773b82b55b1c6db49df0eb120404
Doug Whitmore <dougwhitmore@mac.com>
2015-06-11 17:18:13 -0700

    Initial Commit

---
+++ MultiPartMimeHelper/MultiPartMimeHelper.h
@@ -0,0 +1,19 @@
+//
+//  MultiPartMimeHelper.h
+//  MultiPartMimeHelper
+//
+//  Created by Doug Whitmore on 6/11/15.
+//  Copyright (c) 2015 Good Doug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MultiPartMimeHelper.
+FOUNDATION_EXPORT double MultiPartMimeHelperVersionNumber;
+
+//! Project version string for MultiPartMimeHelper.
+FOUNDATION_EXPORT const unsigned char MultiPartMimeHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiPartMimeHelper/PublicHeader.h>
+
+
